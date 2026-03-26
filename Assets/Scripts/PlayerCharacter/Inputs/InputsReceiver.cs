c7d3870ef0bba6e86634a5f72e3b6f27b2ce31ec
Motek <motek998@gmail.com>
2026-03-26 23:08:28 +0100

    replace rigidbodies & colliders with custom logic, jump & gravity

--- Assets/Scripts/PlayerCharacter/Inputs/InputsReceiver.cs
+++ Assets/Scripts/PlayerCharacter/Inputs/InputsReceiver.cs
@@ -4,10 +4,12 @@ using UnityEngine.InputSystem;
 public class InputsReceiver : MonoBehaviour
 {
     public Vector2 move;
-    private void SetMove(Vector2 newVal)
-    {
-        move = newVal;
-    }
+    public bool jump;
+    private void SetMove(Vector2 newVal) => move = newVal;
+
+    private void SetJump(bool newVal) => jump = newVal;
 
     public void OnMove(InputValue value) => SetMove(value.Get<Vector2>());
+    
+    public void OnJump(InputValue value) => SetJump(value.isPressed);
 }
--- Assets/Scripts/PlayerCharacter/PlayerController.cs
+++ Assets/Scripts/PlayerCharacter/PlayerController.cs
@@ -6,25 +6,82 @@ public class PlayerController : MonoBehaviour
 {
     public InputsReceiver inputs;
 
+    [Header("Bondaries")] 
+    public float horizontalClamp = 8f;
+    public float verticalClamp = -2.5f;
+    
     [Header("Movement")] 
     public float hopTime = 0.2f;
 
     public float hopDistance = 1f;
 
+    public float midAirSpeed = 1f;
+
+    public float gravityForce = -20f;
+    public float jumpForce = 10f;
+
     private Coroutine? moveCoroutine = null;
+    
+    private bool isGrounded = false;
+    private float verticalVelocity = 0f;
 
     void Update()
-    {
-      HorizontalMove();   
+    { 
+        GravityEffect();
+        Jump();
+        HorizontalMove();  
+        VerticalMove();
+        BlockHorizontalMovement();
     }
 
     private void HorizontalMove()
     {
+        
         var moveValue = inputs.move.x;
         
         if (moveValue == 0) return;
+        
+        if (!isGrounded)
+        {
+            transform.position += new Vector3(midAirSpeed * Time.deltaTime * moveValue, 0, 0);
+            return;
+        }
 
         if (moveCoroutine == null) moveCoroutine = StartCoroutine(HopProcedure(moveValue));
+    }
+
+    private void Jump()
+    {
+        if (isGrounded && inputs.jump)
+        {
+            verticalVelocity = jumpForce;
+        }
+    }
+
+    private void GravityEffect()
+    {
+        if (transform.position.y <= verticalClamp)
+        {
+            isGrounded = true;
+            transform.position = new Vector3(transform.position.x, verticalClamp, transform.position.z);
+            verticalVelocity = 0f;
+            return;
+        }
+        
+        verticalVelocity += gravityForce * Time.deltaTime;
+        inputs.jump = false;
+        isGrounded = false;
+    }
+
+    private void VerticalMove()
+    {
+        transform.position += new Vector3(0, verticalVelocity * Time.deltaTime, 0);
+    }
+
+    private void BlockHorizontalMovement()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -horizontalClamp, horizontalClamp),
+            transform.position.y, transform.position.z);
     }
 
     private IEnumerator HopProcedure(float moveVal)
@@ -41,6 +98,11 @@ public class PlayerController : MonoBehaviour
             var xDelta = Mathf.Lerp(0, moveVal * hopDistance, currentProgress);
             
             transform.position = startPos + new Vector3(xDelta, 0, 0);
+
+            if (transform.position.x < -horizontalClamp || transform.position.x > horizontalClamp)
+            {
+                break;
+            }
             
             yield return null;
         }
