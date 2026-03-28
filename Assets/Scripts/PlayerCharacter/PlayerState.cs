a2ce156449b9410c42cb70772c6cd2de8ab6078a
Motek <motek998@gmail.com>
2026-03-28 20:04:08 +0100

    player state

---
+++ Assets/Scripts/PlayerCharacter/PlayerState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayerCharacter
+{
+    public class PlayerState: MonoBehaviour
+    {
+        public int maxHp = 100;
+        public int currentHp;
+
+        private void Awake()
+        {
+            currentHp = maxHp;
+        }
+
+        public void ModifyHp(int amount) => currentHp += amount;
+    }
+}
