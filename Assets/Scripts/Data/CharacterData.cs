a790011838868636368e7d5521aebe601dd64caf
Motek <motek998@gmail.com>
2026-04-10 17:48:35 +0200

    object assets

---
+++ Assets/Scripts/Data/CharacterData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "Character", menuName = "Data/Character")]
+    public class CharacterData: ScriptableObject
+    {
+        public string name;
+        public Sprite sprite;
+        public Sprite portrait;
+    }
+}
---
+++ Assets/Scripts/Data/Characters.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "AllCharacters", menuName = "Data/Characters List")]
+    public class Characters: ScriptableObject
+    {
+        public CharacterData[] characters;
+    }
+}
